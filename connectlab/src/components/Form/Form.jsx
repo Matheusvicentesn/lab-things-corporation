fff4d4c6a6b254412645980432ef8711a6983734
matheusvicentesn <matheusvicentesj5@hotmail.com>
2023-03-07 17:38:20 -0300

    docker file

--- connectlab/src/components/Form/Form.jsx
+++
@@ -1,9 +0,0 @@
-import { FormStyled } from "./Form.styled";
-import PropTypes from "prop-types";
-
-export const Form = ({ children }) => {
-  return <FormStyled>{children}</FormStyled>;
-};
-Form.propTypes = {
-  children: PropTypes.node.isRequired,
-};
